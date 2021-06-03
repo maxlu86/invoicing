41de082f2c792b292d7937bb814f685ce6419ac6
maxlu <maxlu@bk.ru>
2021-06-03 15:38:43 +0200

    API set up, before CORS

--- invoicing-frontend/src/views/Login.vue
+++ invoicing-frontend/src/views/Login.vue
@@ -1,11 +1,34 @@
 <template>
-    <div>Login</div>
+    <div>
+        <pre>{{ user }}</pre>
+        <form @submit.prevent="login">
+            <input v-model="form.email" type="email" placeholder="Email..." /> <br/>
+            <input v-model="form.password" type="password" placeholder="Password ..." />
+            <br />
+            <button>Login</button>
+        </form>
+    </div>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
-        
-    }
+        data: () => ({
+            user: {},
+            form: {
+                email: "max@max.com",
+                password: "12345678"
+            }
+        }),
+        methods: {
+            login() {
+                axios.post('http://localhost:8002/login').then(res=>{
+                    console.log(res);
+                })
+                console.log("Enviando login...", this.form);
+            }
+        },
+    };
 </script>
 
 <style lang="sass" scoped>
