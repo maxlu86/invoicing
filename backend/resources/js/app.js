e921af993df671329ee14ec9862949db7efad3d3
maxlu <maxlu@bk.ru>
2021-06-06 12:57:21 +0200

    VUE laravel settedup

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,1 +1,17 @@
+
+
 require('./bootstrap');
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+
+const app = new Vue({
+    el: '#app',
+});
